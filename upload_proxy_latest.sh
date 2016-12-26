ca78d6f1c0457880015af5b071496212189ad30a
Mark Fickett <mark.fickett@gmail.com>
2016-12-25 20:49:59 -0500

    Use IMG_BASE in proxy-res upload script.

--- upload_proxy_latest.sh
+++ upload_proxy_latest.sh
@@ -9,15 +9,19 @@ PROXY_SIZE=1296
 
 # See capture_and_upload.sh docs for project.sh.
 source $TIMELAPSE_DIR/project.sh
+if [ -z "$IMG_BASE" ]
+then
+  IMG_BASE=$TIMELAPSE_DIR
+fi
 
-newest=`ls $TIMELAPSE_DIR/$PROJECT/*/*/*/* | tail -1`
+newest=`ls $IMG_BASE/$PROJECT/*/*/*/* | tail -1`
 tmp_file=`mktemp -t pitimelapse.XXXXXX`
 # sudo apt-get install imagemagick
 convert \
     -resize ${PROXY_SIZE}x${PROXY_SIZE} \
     $newest \
     $tmp_file
-proxy_name=`echo ${newest#$TIMELAPSE_DIR/$PROJECT/} | tr '/' '_'`
+proxy_name=`echo ${newest#$IMG_BASE/$PROJECT/} | tr '/' '_'`
 # Use `ssh user@host 'mkdir -p path` for first-time setup.
 scp $tmp_file ${REMOTE_PATH}/${PROJECT}_${PROXY_SIZE}/$proxy_name
 rm $tmp_file
